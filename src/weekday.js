c6a8ef143ef8bbc5e431551b12282c1fb08872ee
Jack Jones <jacktjones4@gmail.com>
2022-02-02 10:29:13 -0800

    test one passed correctly Co-Authored-By Jake Haley <jakechaley@gmail.com>

--- src/weekday.js
+++ src/weekday.js
@@ -1,8 +1,8 @@
 export default class Date {
   constructor (month, day, year) {
-    // this.month = month;
-    // this.day = day;
-    // this.year = year;
+    this.month = month;
+    this.day = day;
+    this.year = year;
     }
     
 };
