1b7b742201657e15b3e74c6aabf2936e7b649bea
jakechaley <jakechaley@gmail.com>
2022-02-03 14:26:07 -0800

    second test passing. Should return day of week corresponding to index number
    Co-Authored-by: Jessica Baker <jessb.epicodus@gmail.com>
    Co-Authored-By: Jack Jones <jacktjones4@gmail.com>

--- __tests__/weekday.test.js
+++ __tests__/weekday.test.js
@@ -1,19 +1,15 @@
 import { TestScheduler } from 'jest';
 import dayOfWeek from "./../src/weekday.js";
 
-
 describe('dayOfWeek', () => {
   test('should return day of the week in number from 0-6', () => {
     const newDay = new Date('02/02/2022');
     const result = dayOfWeek(newDay);
     expect(result).toEqual(3);
   });
+});
   test('should return day of the week corresponding to the index number', () => {
     const newDay = new Date('02/02/2022');
     const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     expect(weekDay[newDay.getDay()]).toEqual('Wednesday');
   });
-});
-
-
-
--- src/weekday.js
+++ src/weekday.js
@@ -1,4 +1,4 @@
 export default function dayOfWeek(newDay) {
+  const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   return newDay.getDay();
 }
-
